f0db8d17ab2b0af1b49630e7683ada49342a700e
LeeHyeWoun <silro812@naver.com>
2020-04-25 22:48:40 +0900

    The script is complete.

--- SettingController.cs
+++ SettingController.cs
@@ -1,11 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SettingController : MonoBehaviour {
+﻿/**
+ * Date     : 2020.04.25
+ * Manager  : 이혜원
+ * 
+ * The function of this script :
+ *  Scene "14_Setting"의 모든 이벤트를 담당하는 스크립트
+ */
+public class SettingController : SoundController {
 
     public void BE_GoMain() {
-        //SoundManager.Instance
+        SoundManager.Instance.Play_effect(0);
+        SceneController.Instance.Load_Scene(0);
+    }
+    public void BE_Play() {
+        SoundManager.Instance.Play_effect(0);
+        SceneController.Instance.Destroy_Scene();
     }
 
 }
--- SoundController.cs
+++ SoundController.cs
@@ -6,20 +6,18 @@ using UnityEngine.UI;
  * Manager  : 이혜원
  * 
  * The function of this script :
- *  소리에 관한 다양한 이벤트를 다루는 스크립트
- *  
- *  2020.04.15 수정 사항 : 
- *  SoundManager와 같은 위치에 스크립트를 적용, MainManager, MapManager등 각 씬의 Manager가 씬이 로드될 때 최초로 한번만 SoundManager를 Find하여 사용
+ *  슬라이더를 통해 소리의 설정 값을 다루는 스크립트
  *  
  *  Applied Location :
- *  -> SoundManager(빈 오브젝트)
+ *  -> SettingController에 상속
  */
 public class SoundController : MonoBehaviour
 {
     //객체 할당
     public Slider s_bgm, s_effect;
 
-    private AudioSource bgm, effect;
+    private AudioSource bgm = SoundManager.Instance.Player_BGM;
+    private AudioSource effect = SoundManager.Instance.Player_effect;
     
     //변수
     private float value_effect = 1f;
@@ -28,61 +26,24 @@ public class SoundController : MonoBehaviour
     //초기화
     void Start()
     {
-        if (SoundManager.Instance)
-        {
-            bgm = SoundManager.Instance.Player_BGM;
-            effect = SoundManager.Instance.Player_effect;
-
-            //슬라이더 초기화
-            if (s_bgm != null || s_effect != null) {
-                s_bgm.value = bgm.volume;
-                s_effect.value = effect.volume;
-            }
-        }
-        else
-            print("SoundManager 없음\n소리 설정을 원한다면 '00_Main'로 가주세요.");
+        //슬라이더 초기화
+        s_bgm.value = bgm.volume;
+        s_effect.value = effect.volume;
     }
 
     //배경음악 설정
     public void SetBGM()
     {
-        if (SoundManager.Instance)
-        {
-            value_bgm = s_bgm.value;
-            bgm.volume = value_bgm;
-            PlayerPrefs.SetFloat("tmp_bgm", value_bgm);
-        }
-        else {
-            s_bgm.value = 0;
-            print("'SoundManager'가 없어 소리 설정이 불가능합니다. ");
-        }
+        value_bgm = s_bgm.value;
+        bgm.volume = value_bgm;
+        PlayerPrefs.SetFloat("tmp_bgm", value_bgm);
     }
 
     //효과음 설정
     public void SetEffect()
     {
-        if (SoundManager.Instance)
-        {
-            value_effect = s_effect.value;
-            effect.volume = value_effect;
-            PlayerPrefs.SetFloat("effect", value_effect);
-        }
-        else {
-            s_effect.value = 0;
-            print("'SoundManager'가 없어 소리 설정이 불가능합니다. ");
-        }
+        value_effect = s_effect.value;
+        effect.volume = value_effect;
+        PlayerPrefs.SetFloat("effect", value_effect);
     }
-
-    //Effect 선택 Play
-    public void Play_effect(int num)
-    {
-        if (SoundManager.Instance)
-            SoundManager.Instance.Play_effect(num);
-    }
-
-    //BGM 소리 크기 설정
-    public void SetBGM_Volum(float v) {
-        bgm.volume = v;
-    }
-
 }
