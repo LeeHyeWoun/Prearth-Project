9ab58a7bc7fe2554afac176f29681934742c8e58
LeeHyeWoun <silro812@naver.com>
2020-04-25 21:36:57 +0900

    The script is almost complete.

--- SceneController.cs
+++ SceneController.cs
@@ -11,180 +11,115 @@ using UnityEngine;
  *  Applied Location :
  *  -> 모든 Scene의 GameDirector(빈 오브젝트)
  */
-
 public class SceneController : MonoBehaviour {
 
-    //상수
-    const string MAP_NUMBER = "TheMapIs";
-    const string PRE_SCENE = "PreScene";
+    //싱글톤 인스턴스
+    public static SceneController Instance = null;
 
     //변수
-    string current;
+    private int planet_num = 0; //토양 : 0, 수질 : 1, 대기 : 2
+    private ToastManager TM;
 
-    void Awake()
-    {
-        current = SceneManager.GetActiveScene().name;
+    private void Awake() {
+        TM = GetComponent<ToastManager>();
     }
 
-    //씬 이동에 사용
-    void Go(string scene) {
-        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    //행성값에 대한 설정자
+    public void SetPlanetNum(int num) {
+        planet_num = num;
     }
 
-    //씬 이름 앞의 숫자를 통해 구분
-    public void Go(int num) {
-        string scene;
-        switch (num) {
-            case 1: // 01_Main
-                scene = "01_Main";
-                //메인용 배경음악으로 초기화
-                if (GetActiveScene_num() > 10)
-                    SoundManager.Instance.BGM_reset();
-                break;
-            case 2: // 02_Map.............. soil 세팅
-                scene = "02_Map";
-                PlayerPrefs.SetInt(MAP_NUMBER, 1);
-                break;
-            case 3: // 02_Map.............. water 세팅
-                scene = "02_Map";
-                PlayerPrefs.SetInt(MAP_NUMBER, 2);
-                break;
-            case 4: // 02_Map.............. air 세팅 
-                scene = "02_Map";
-                PlayerPrefs.SetInt(MAP_NUMBER, 3);
-                break;
-            case 5: // 05_Do_planet
-                Change_Scene(5);
-                return;
-            case 6: // 06_Do_item
-                Change_Scene(6);
-                return;
-            case 7: // 07_Do_diary
-                Change_Scene(7);
-                return;
+    //행성값에 대한 접근자
+    public int GetPlanetNum() {
+        return planet_num;
+    }
 
-            default:
-                // 게임 씬
-                /*
-                 * 02_Map의 버튼 'mapPoint1'에 num = 11
-                 * 02_Map의 버튼 'mapPoint2'에 num = 12
-                 * 02_Map의 버튼 'mapPoint3'에 num = 13
-                 * 를 적용하고 TheMapIs 값에 따라 결과를 달리한다.
-                 */
-                if (num > 10 && num < 14)
-                {
-                    int mapNum = PlayerPrefs.GetInt(MAP_NUMBER, 1);
-                    scene = (num + 3 * (mapNum - 1)).ToString();
-                    if (mapNum == 1)
-                        scene += "_Soil_";
-                    else if (mapNum == 2)
-                        scene += "_Water_";
-                    else
-                        scene += "_Air_";
-                    scene += (num - 10).ToString();
+    private int Index_padding(ref int num) {   //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!게임 씬 추가 후 삭제 혹은 수정 바람
 
-                    //아직 준비되지 않은 게임씬일 경우
-                    if (!Application.CanStreamedLevelBeLoaded(scene)) {
-                        string message = "'stage" + (num - 10).ToString() + "'는 아직 준비중입니다. (" + scene + ")";
-                        GameObject GO = GameObject.Find("Default");
-                        if (GO)
-                            GO.GetComponent<ToastManager>().ShowToast(message);
-                        return;
-                    }
+        if (num > 9)
+            num--;
+        if (num > 8)
+            num--;
+        if (num > 6)
+            num--;
+        if (num > 5)
+            num--;
+        if (num > 3)
+            num--;
 
-                    //게임용 BGM설정
-                    SoundManager.Instance.BGM_chage(true);
-                }
-                // 존재하지 않는 씬 에러처리
-                else {
-                    scene = "01_Main";
-                    print("해당 씬<" + scene + ">은 존재하지 않습니다.");
-                    return;
-                }
-                break;
+        return num;
+    }
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    //Scene 이동 (Single mode)
+    //주의 사항 : 빌드세팅의 인덱스 설정
+    public void Load_Scene(int num) {
+
+        //Scene 존재 여부 체크
+        if (num == 3 || num == 5 || num == 6 || num == 8 || num == 9) //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!게임 씬 추가 후 삭제 혹은 수정 바람
+        {
+            TM.ShowToast("아직 준비되지 않은 Scene입니다.");
+            return;
         }
-        Go(scene);
-    }
 
-    //조사일지 버튼에 적용
-    public void Add_Scene(int num) {
-        //기본 Scene은 일시 정지
-        Time.timeScale = 0;
+        Index_padding(ref num);
+        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-        string scene = "";
-        switch (num) {
-            case 5:
-                scene = "05_Do_planet";
-                break;
-            case 8:
-                scene = "08_Setting";
-                break;
-            case 9:
-                scene = "09_Dialog";
-                break;
-            case 10:
-                scene = "10_Clear";
-                break;
+        if (num > 11- 5/*게임 씬 추가 후  수정 바람*/)
+        {
+            //일시정지
+            Time.timeScale = 0;
+
+            //이미 Additive Scene이 있다면 Unload 하기
+            if (SceneManager.sceneCount > 1)
+                SceneManager.UnloadSceneAsync(SceneManager.GetSceneAt(SceneManager.sceneCount-1).name);
+
+            //추가
+            SceneManager.LoadSceneAsync(num, LoadSceneMode.Additive);
         }
-        if (scene.Length > 0)
-            SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+        else if (num < 11- 5/*게임 씬 추가 후  수정 바람*/)
+        {
+            //배경음악 설정
+            if (num == 0 && GetActiveScene_num() > 1)
+                SoundManager.Instance.BGM_reset();
+            else if (num > 1 && num < 11)
+                SoundManager.Instance.BGM_chage(true);
+
+            //이동
+            SceneManager.LoadScene(num, LoadSceneMode.Single);
+        }
+        else
+        {
+            //일시정지
+            Time.timeScale = 0;
+
+            //추가
+            SceneManager.LoadSceneAsync(num, LoadSceneMode.Additive);
+            SceneManager.LoadSceneAsync(num + 1, LoadSceneMode.Additive);
+        }
     }
 
     //나가기(뒤로가기) 버튼에 적용
     public void Destroy_Scene() {
-        //기본 Scene 다시 작동
+
+        //일시정지 해제
         Time.timeScale = 1;
+
+        //Active Scene을 제외한 나머지 Scene Unload
         for (int i = 1; i < SceneManager.sceneCount; ++i)
-        {
             SceneManager.UnloadSceneAsync(SceneManager.GetSceneAt(i).name);
-        }
-    }
-
-    public void Change_Scene(int num) {
-        string scene;
-        switch (num)
-        {
-            case 5:
-                scene = "05_Do_planet";
-                break;
-            case 6:
-                scene = "06_Do_item";
-                break;
-            case 7:
-                scene = "07_Do_diary";
-                break;
-            case 10:
-                scene = "10_Clear";
-                break;
-            default:
-                scene = "";
-                break;
-        }
-        if (scene.Length > 0) {
-            SceneManager.LoadScene(scene, LoadSceneMode.Additive);
-            for (int i = 1; i < SceneManager.sceneCount - 1; ++i)
-            {
-                SceneManager.UnloadSceneAsync(SceneManager.GetSceneAt(i).name);
-            }
-            print("change " + scene);
-        }
-    }
-
-    public void BackMap()
-    {
-        StopAllCoroutines();
-        int map = PlayerPrefs.GetInt("TheMapIs", 1) + 1;
-        Go(map);
     }
 
     public string GetActiveScene_name() {
-        return current;
+        return SceneManager.GetActiveScene().name;
     }
 
     public int GetActiveScene_num() {
-        string current_num = current.Substring(0, 2); 
-        return int.Parse(current_num);
+        return SceneManager.GetActiveScene().buildIndex;
     }
 
-
+    //MapManager에서 사용
+    public void Prevent(int num) {
+        TM.ShowToast("'Stage" + num + "'부터 입장해주세요!");
+    }
 }
