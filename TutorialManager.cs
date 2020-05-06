1ff1c0abe67f675f6bd8c46570de55fade2eb3b4
LeeHyeWoun <silro812@naver.com>
2020-05-06 14:26:14 +0900

    Add Tutorial 5 and 6

--- TutorialManager.cs
+++ TutorialManager.cs
@@ -11,7 +11,7 @@ using UnityEngine.UI;
 public class TutorialManager : MonoBehaviour {
 
     //할당 받을 객체
-    public Texture tutorial1, tutorial2, tutorial3, tutorial4;
+    public Texture tutorial1, tutorial2, tutorial3, tutorial4, tutorial5, tutorial6;
     public RawImage RI_tutorial;
     public GameObject arrow_pre, arrow_next;
 
@@ -24,31 +24,48 @@ public class TutorialManager : MonoBehaviour {
     void Start() {
         Time.timeScale = 0;
         SM = SoundManager.Instance;
+
+        if (SceneController.Instance.GetActiveScene_num().Equals(1))
+            ChageImage(page = 5);
     }
 
     public void Next() {
         SM.Play_effect(0);
-        ChageImage(++page);
 
-        if (page.Equals(2))
+        if (page.Equals(1))
             arrow_pre.SetActive(true);
-        else if (page.Equals(4))
+        else if (page.Equals(3))
             arrow_next.SetActive(false);
+        else if (page.Equals(5))
+        {
+            arrow_pre.SetActive(true);
+            arrow_next.SetActive(false);
+        }
+
+        ChageImage(++page);
     }
 
     public void Pre() {
         SM.Play_effect(0);
-        ChageImage(--page);
-        if (page.Equals(1))
+
+        if (page.Equals(2))
             arrow_pre.SetActive(false);
-        else if (page.Equals(3))
+        else if (page.Equals(4))
             arrow_next.SetActive(true);
+        else if (page.Equals(6))
+        {
+            arrow_pre.SetActive(false);
+            arrow_next.SetActive(true);
+        }
+
+        ChageImage(--page);
     }
 
     public void Skip()
     {
         SM.Play_effect(0);
-        PlayerPrefs.SetInt("tmp_Clear", 0);
+
+        PlayerPrefs.SetInt("tmp_Clear", page < 5 ? -1 : 0);
         Time.timeScale = 1;
         SceneController.Instance.Destroy_Scene();
     }
@@ -67,6 +84,14 @@ public class TutorialManager : MonoBehaviour {
 
             case 4:
                 texture = tutorial4;
+                break;
+
+            case 5:
+                texture = tutorial5;
+                break;
+
+            case 6:
+                texture = tutorial6;
                 break;
 
             default:
