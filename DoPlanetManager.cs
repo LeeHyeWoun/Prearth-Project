62eaca24c8b7d70f291a40ced7bf91d2c84972c4
LeeHyeWoun <silro812@naver.com>
2020-05-13 19:10:38 +0900

    The script is complete.

--- DoPlanetManager.cs
+++ DoPlanetManager.cs
@@ -23,30 +23,42 @@ public class DoPlanetManager : MonoBehaviour {
 
         int clear = PlayerPrefs.GetInt("tmp_Clear",0);
 
+        //첫번째 이미지 설정-----------------------------------------------------------------------
+        //미진행
         if (clear < 0)
             return;
+        //진행중
         else if (clear < 3)
             RIs_planet[0].texture = Texturex_planet[0];
+        //완료
         else
         {
             RIs_planet[0].texture = Texturex_planet[1];
             Txts_name[0].text = PlayerPrefs.GetString("tmp_date_soil","null") + " 조사완료";
         }
 
+        //두번째 이미지 설정-----------------------------------------------------------------------
+        //미진행
         if (clear < 3)
             return;
+        //진행중
         else if (clear < 6)
             RIs_planet[1].texture = Texturex_planet[2];
+        //완료
         else
         {
             RIs_planet[1].texture = Texturex_planet[3];
             Txts_name[1].text = PlayerPrefs.GetString("tmp_date_water", "null") + " 조사완료";
         }
 
+        //세번째 이미지 설정-----------------------------------------------------------------------
+        //미진행
         if (clear < 6)
             return;
+        //진행중
         else if (clear < 9)
             RIs_planet[2].texture = Texturex_planet[4];
+        //완료
         else
         {
             RIs_planet[2].texture = Texturex_planet[5];
